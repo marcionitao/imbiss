09280cfff6bb169ea5492475d60260605909dc49
Marcio Nitao <marcionitao@gmail.com>
2018-05-31 14:35:22 +0200

    config database provider

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -1,17 +1,64 @@
-import { HttpClient } from '@angular/common/http';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the DatabaseProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DatabaseProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello DatabaseProvider Provider');
+  constructor(private sqlite: SQLite) { }
+
+  // get datas in DB
+  public getDB() {
+    // para cada opearção é necessario abrir o DB
+    return this.sqlite.create({
+      name: 'product.db',
+      location: 'default'
+    })
+
+  }
+  // Creates a bank if there is no existing bank with the name in the parameter
+  public createDB() {
+    return this.getDB()
+      .then((db: SQLiteObject) => {
+
+         // Create Tables
+         this.createTables(db);
+
+         // Insert datas default
+         this.insertDefaultItems(db);
+
+      })
+      .catch(e => console.error(e));
   }
 
+  // creating the tables in the database
+  private createTables(db: SQLiteObject){
+     // Create Tables
+     db.sqlBatch([
+      ['CREATE TABLE IF NOT EXISTS categories (id integer primary key AUTOINCREMENT NOT NULL, name TEXT)'],
+      ['CREATE TABLE IF NOT EXISTS products (id integer primary key AUTOINCREMENT NOT NULL, name TEXT, price EUR, duedate DATE, active integer, category_id integer, FOREIGN KEY(category_id) REFERENCES categories(id))']
+    ])
+      .then(() => console.log('Tabelas criadas'))
+      .catch(e => console.error('Erro ao criar as tabelas', e));
+  }
+
+  // Including the default data
+  private insertDefaultItems(db: SQLiteObject){
+    db.executeSql('select COUNT(id) as qtd from categories', {})
+    .then((data: any) => {
+      //If there is no record
+      if (data.rows.item(0).qtd == 0) {
+
+        // Create tables
+        db.sqlBatch([
+          ['insert into categories (name) values (?)', ['Hambúrgueres']],
+          ['insert into categories (name) values (?)', ['Bebidas']],
+          ['insert into categories (name) values (?)', ['Sobremesas']]
+        ])
+          .then(() => console.log('Dados padrões incluídos'))
+          .catch(e => console.error('Erro ao incluir dados padrões', e));
+
+      }
+    })
+    .catch(e => console.error('Erro ao consultar a qtd de categorias', e));
+  }
 }
