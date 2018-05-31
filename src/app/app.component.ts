d0207d8a35661d489fadd4cff729091b9d147d44
Marcio Nitao <marcionitao@gmail.com>
2018-05-31 14:54:20 +0200

    config appComponent to run app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,41 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DatabaseProvider } from '../providers/database/database'
 
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  // para evitar problemas no arranque da DB, deixei a rootPage como null
+  rootPage: any = null;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, dbProvider: DatabaseProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      //create DB
+      dbProvider.createDB()
+        .then(() => {
+          // fechando a SplashScreen somente quando o banco for criado
+          this.openHomePage(splashScreen);
+        })
+        .catch(() => {
+          // ou se houver erro na criação do banco
+          this.openHomePage(splashScreen);
+        });
     });
+  }
+
+  // depois que tudo inicializar(DB), ai sim arranca a HomePage
+  private openHomePage(splashScreen: SplashScreen) {
+    splashScreen.hide();
+    this.rootPage = HomePage;
   }
 }
 
