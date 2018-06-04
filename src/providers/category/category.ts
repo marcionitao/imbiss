a91dd8a2d128e15eb69858f59177ac15378ded9b
Marcio Nitao <marcionitao@gmail.com>
2018-06-04 06:33:44 +0200

    add category

--- src/providers/category/category.ts
+++ src/providers/category/category.ts
@@ -1,17 +1,34 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SQLiteObject } from '@ionic-native/sqlite';
+import { DatabaseProvider } from '../database/database';
+import { Product } from '../../model/product';
 
-/*
-  Generated class for the CategoryProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class CategoryProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello CategoryProvider Provider');
+  constructor(private dbProvider: DatabaseProvider) { }
+
+  public getAll() {
+    return this.dbProvider.getDB()
+      .then((db: SQLiteObject) => {
+        let sql = 'SELECT * FROM categories';
+
+        return db.executeSql(sql, [])
+          .then((data: any) => {
+            if (data.rows.length > 0) {
+              let categories: any[] = [];
+              for (var i = 0; i < data.rows.length; i++) {
+                var category = data.rows.item(i);
+                categories.push(category);
+              }
+              return categories;
+            } else {
+              return [];
+            }
+          })
+          .catch((e) => console.error(e));
+      })
+      .catch((e) => console.error(e));
   }
 
 }
